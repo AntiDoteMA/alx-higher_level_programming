a6e69663b2d944f3166348d7ef2934798c494160
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-06-13 17:57:24 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx >= 0 and idx < len(my_list):
-        return my_list[idx]
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
