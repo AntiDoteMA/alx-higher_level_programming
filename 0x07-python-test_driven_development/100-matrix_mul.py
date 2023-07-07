aca4d7479bb9f8b2f82d22397c3ab4cfb65d2aa3
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-07-06 17:54:52 -0700

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -56,16 +56,16 @@ def matrix_mul(m_a, m_b):
     mul_matrix = []
 
     for row_1 in m_a:
-        l = 0
+        lenx = 0
         l_row = []
-        while l < len(m_b[0]):
+        while lenx < len(m_b[0]):
             result = 0
             k = 0
             for column_1 in row_1:
                 result += column_1 * m_b[k][l]
                 k += 1
             l_row.append(result)
-            l += 1
+            lenx += 1
         mul_matrix.append(l_row)
 
     return mul_matrix
